2c6b355faeba1b8a00438213a6d8c88d8661ee22
aakashtapadiya <aakashtapadiya28253@gmail.com>
2024-07-27 18:58:15 +0530

    first commit

---
+++ backend/src/main/java/com/ecommerce/plantme/CruddemoApplication.java
@@ -0,0 +1,23 @@
+package com.ecommerce.plantme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+/*	@Bean
+	public CommandLineRunner commandLineRunner() {
+
+		return runner-> {
+		}
+ */
+
+}
